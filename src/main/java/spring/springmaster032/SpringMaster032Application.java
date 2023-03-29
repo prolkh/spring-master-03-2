bbba117da58cef7f2a825ba567b97d93843fc0a5
prolkh <prolkh@gmail.com>
2023-03-29 23:34:38 +0900

    Spring MVC - Basic functionality project creation

---
+++ src/main/java/spring/springmaster032/SpringMaster032Application.java
@@ -0,0 +1,13 @@
+package spring.springmaster032;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMaster032Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMaster032Application.class, args);
+	}
+
+}
---
+++ src/test/java/spring/springmaster032/SpringMaster032ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.springmaster032;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMaster032ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
